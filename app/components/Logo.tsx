7e842422602e69d275703120574db18cae4d2ca4
Eric Maya <ericmaya8a@gmail.com>
2023-12-12 16:58:23 -0600

    chore: update next config

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../../public/images/logo.png";
-import { urls } from "../constants";
+import { constants, urls } from "../constants";
 
 const logoSizes: { [key: string]: number } = {
   xs: 40,
@@ -24,10 +24,12 @@ export function Logo({
   isLink,
   size = "md",
 }: LogoProps) {
+  const src = `${constants.basePath}${logo.src}`;
   const imageProps = {
     className,
+    height: 10,
     priority: true,
-    src: logo,
+    src,
     width: logoSizes[size],
   };
 
--- app/constants/text.ts
+++ app/constants/text.ts
@@ -1,3 +1,6 @@
+import config from "../../next.config";
+
 export const constants = {
   appName: "Aiki Martial Arts",
+  basePath: config.basePath,
 } as const;
